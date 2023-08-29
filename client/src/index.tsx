9b4b6bf6474acfc1b7466008875c6e05b7227cdb
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2023-08-29 23:42:51 +0300

    Remove defailt styles and unused code

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
