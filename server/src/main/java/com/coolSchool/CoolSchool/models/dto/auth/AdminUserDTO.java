8a625d6bd10d5fdaddc168100c6a2e2358b7d884
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-10-26 21:13:04 +0300

    add tests

--- server/src/main/java/com/coolSchool/CoolSchool/models/dto/auth/AdminUserDTO.java
+++ server/src/main/java/com/coolSchool/CoolSchool/models/dto/auth/AdminUserDTO.java
@@ -1,7 +1,10 @@
 package com.coolSchool.CoolSchool.models.dto.auth;
 
 import com.coolSchool.CoolSchool.models.entity.File;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
