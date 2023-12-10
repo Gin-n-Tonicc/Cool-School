58b777da24cdf9f4f66acf6877bfbc977d0dbd1d
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-12-10 13:43:00 +0200

    fix GitHub build

--- server/src/test/java/com/coolSchool/CoolSchool/CoolSchoolApplicationTests.java
+++ server/src/test/java/com/coolSchool/CoolSchool/CoolSchoolApplicationTests.java
@@ -1,9 +1,9 @@
 package com.coolSchool.CoolSchool;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class CoolSchoolApplicationTests {
 
     @Test
