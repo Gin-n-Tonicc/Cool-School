c43c763a937d24c5adc9f11593be00f126fed6c0
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2024-01-20 17:32:04 +0200

    insert migrations for courses

---
+++ server/src/main/resources/db/migration/V30__Insert_Resouses.sql
@@ -0,0 +1,9 @@
+INSERT INTO resources (name, file_id, subsection_id, is_deleted)
+VALUES
+  ('DATA TYPES PRESENTATION', 6, 1, 0),
+  ('SEMANTICS MODEL', 7, 2, 0),
+  ('DATA TYPES', 8, 1, 0),
+  ('C# FEATURES', 9, 2, 0),
+  ('HORNER WIKIPEDIA INFORMATION', 10, 3, 0),
+  ('HORNER METHOD', 11, 3, 0),
+  ('POLYNOMIAL', 12, 3, 0);
