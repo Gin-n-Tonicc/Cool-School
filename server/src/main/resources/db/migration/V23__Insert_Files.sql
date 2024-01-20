3e5af73da19f4989948be0f424335839cb3db3a8
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2024-01-20 15:36:59 +0200

    insert migrations for blogs

---
+++ server/src/main/resources/db/migration/V23__Insert_Files.sql
@@ -0,0 +1,4 @@
+INSERT INTO files (name, url, type, is_deleted) VALUES
+('create blog img.jpg', 'server/src/main/resources/static/uploads/0beab526-c625-4ca1-8b89-c480c6d0adcf_create blog img.jpg', 'image/jpeg', 0),
+('rsz_429459_d2.jpg', 'server/src/main/resources/static/uploads/27a0e965-4291-483a-98a7-ac6a06a7e40c_rsz_429459_d2.jpg', 'image/jpeg', 0),
+('rsz_img_20230205_172426.jpg', 'server/src/main/resources/static/uploads/b383f5e9-cd12-4942-882f-e88bd1133e7e_rsz_img_20230205_172426.jpg', 'image/jpeg', 0);
