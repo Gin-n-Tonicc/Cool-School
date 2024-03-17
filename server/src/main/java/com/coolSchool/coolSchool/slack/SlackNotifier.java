e9feb345e2dabc17e38836d3a545775a0027639e
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2024-03-17 11:41:59 +0200

    add comments to SlackNotifier, ApiExceptionParser, CookieHelper and ObjectMapperHelper

--- server/src/main/java/com/coolSchool/coolSchool/slack/SlackNotifier.java
+++ server/src/main/java/com/coolSchool/coolSchool/slack/SlackNotifier.java
@@ -10,6 +10,10 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * SlackNotifier is a component responsible for sending notifications to Slack channels.
+ * It utilizes a webhook URL configured in the application properties to send messages to Slack.
+ */
 @Component
 @Slf4j
 public class SlackNotifier {
@@ -18,14 +22,17 @@ public class SlackNotifier {
     private String webhookUrl;
 
     public void sendNotification(String message) {
+        // Build the payload with the message
         Payload payload = Payload.builder()
                 .text(message)
                 .build();
 
         try {
+            // Send the payload to Slack using the configured webhook URL
             Slack slack = Slack.getInstance();
             slack.send(webhookUrl, payload);
         } catch (IOException ex) {
+            // Log any errors that occur during the notification process
             LocalDateTime timestamp = LocalDateTime.now();
             String time = String.format("%s%n", DATE_TIME_FORMATTER.format(timestamp));
             log.warn(time + ex.getMessage());
