45738bd5c12d1e9dfa72fccb5f8d6b51bc5dfc09
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2024-03-17 20:18:27 +0200

    Add missing internationalisation

--- client/src/components/not-found/NotFound.tsx
+++ client/src/components/not-found/NotFound.tsx
@@ -1,17 +1,20 @@
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { PagesEnum } from '../../types/enums/PagesEnum';
 import './NotFound.scss';
 
 // The component that displays the notfound page which links to home
 export default function NotFound() {
+  const { t } = useTranslation();
+
   return (
     <div id="notfound">
       <div className="notfound">
         <div className="notfound-404">
-          <h1>Oops!</h1>
-          <h2>404 - The Page can't be found</h2>
+          <h1>{t('not.found.title')}</h1>
+          <h2>404 - {t('not.found.subtitle')}</h2>
         </div>
-        <Link to={PagesEnum.Home}>Go TO Homepage</Link>
+        <Link to={PagesEnum.Home}>{t('not.found.text')}</Link>
       </div>
     </div>
   );
